d3b82a6fd111c95fe13e14fcadc490e54c9371c3
Waiz Monazzum <waizmonazzum270@gmail.com>
2025-09-19 13:23:19 +0000

    Chalo perfect...to thik hai ab mai ye app ko apna public repository me p

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,15 +3,16 @@ import { initializeApp, getApps } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration is now sourced from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyATUcEV5XGgj5oMkAv1a5Xh-6jZApOXVBw",
-  authDomain: "mindmate-80e5c.firebaseapp.com",
-  projectId: "mindmate-80e5c",
-  storageBucket: "mindmate-80e5c.appspot.com",
-  messagingSenderId: "1040365164281",
-  appId: "1:1040365164281:web:3cf995fb97fe775c33b428"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 let app;
